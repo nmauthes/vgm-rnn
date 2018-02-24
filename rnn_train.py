fae907594bb5f198559a4622381051e3e505f198
Nicolas Mauthes <nmauthes@netscape.net>
2018-02-23 17:54:15 -0800

    update gitignore

--- rnn_train.py
+++ rnn_train.py
@@ -30,23 +30,28 @@ MAX_DURATION = SUBDIVISION * 4 # Corresponds to 1 whole note
 
 
 if __name__ == '__main__':
-    training_data = np.empty(shape=(128, 0, 2))
+    if os.path.exists(TRAINING_DATA_PATH):
+        training_data = np.load(TRAINING_DATA_PATH)
+    else:
+        training_data = np.empty(shape=(128, 0, 2))
 
-    errors = 0
-    for i, folder in enumerate(DATA_FOLDERS):
-        print(f'Processing folder \'{folder}\' ({i + 1}/{len(DATA_FOLDERS)})')
+        errors = 0
+        for i, folder in enumerate(DATA_FOLDERS):
+            print(f'Processing folder \'{folder}\' ({i + 1}/{len(DATA_FOLDERS)})')
 
-        if os.path.exists(folder):
-            midis = filter_midi_files(folder, ALLOWED_TIME_SIGS, ALLOWED_KEYS)
+            if os.path.exists(folder):
+                midis = filter_midi_files(folder, ALLOWED_TIME_SIGS, ALLOWED_KEYS)
 
-            for mid in midis:
-                try:
-                    arr = pretty_midi_to_numpy_array(mid, subdivision=SUBDIVISION, transpose_notes=True)
-                    training_data = np.concatenate((training_data,arr), axis=1)
-                except MIDIError:
-                    errors += 1
+                for mid in midis:
+                    try:
+                        arr = pretty_midi_to_numpy_array(mid, subdivision=SUBDIVISION, transpose_notes=True)
+                        training_data = np.concatenate((training_data,arr), axis=1)
+                    except MIDIError:
+                        errors += 1
+            else:
+                raise Exception('Data folder not found!')
 
-        else:
-            raise Exception('Data folder not found!')
+            np.save(TRAINING_DATA_PATH, training_data) # Serialize array of training data for future use
 
-        np.save(TRAINING_DATA_PATH, training_data)
+    print(training_data.shape)
+
