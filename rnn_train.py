7ba937d7f1e00343c325daacebc0c8e8b541b523
Nicolas Mauthes <nmauthes@netscape.net>
2018-03-02 17:21:14 -0800

    update requirements.txt

--- rnn_train.py
+++ rnn_train.py
@@ -9,7 +9,9 @@ import argparse
 
 import numpy as np
 
-from midi_parser import MIDIError, filter_midi_files, pretty_midi_to_numpy_array, numpy_array_to_pretty_midi
+import keras
+
+from midi_parser import MIDIError, filter_midi_files, pretty_midi_to_numpy_array
 
 
 DATA_FOLDERS = ['nes_data']
@@ -31,9 +33,11 @@ MAX_DURATION = SUBDIVISION * 4 # Corresponds to 1 whole note
 
 if __name__ == '__main__':
     if os.path.exists(TRAINING_DATA_PATH):
-        training_data = np.load(TRAINING_DATA_PATH)
+        print('Loading training data...')
+        training_data = np.load(TRAINING_DATA_PATH) # Load saved training data if available
     else:
-        training_data = np.empty(shape=(128, 0, 2))
+        print('Building training set. This might take a while...')
+        training_data = np.empty(shape=(128, 0, 2)) # Otherwise gather MIDIs and build training set
 
         errors = 0
         for i, folder in enumerate(DATA_FOLDERS):
